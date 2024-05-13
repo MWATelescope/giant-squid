abefea613095d510777ac2c9350b3e6c1d142037
Greg Sleap <greg.sleap@curtin.edu.au>
2024-05-13 15:45:38 +1000

    Attempt to fix release CI

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -6,12 +6,7 @@ set -eux
 # release tarballs.
 cp .github/workflows/releases-readme.md README.md
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    source /root/.cargo/env
-    
-    rustup install 1.70 --no-self-update
-    rustup default 1.70
-
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then    
     # Build a release for each x86_64 microarchitecture level. v4 can't be
     # compiled on GitHub for some reason.
     for level in "x86-64" "x86-64-v2" "x86-64-v3"; do
