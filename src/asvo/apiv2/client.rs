b87a0a882f6c1e5a6c3194d52142a0faeb16136c
Greg Sleap <greg.sleap@curtin.edu.au>
2026-09-15 13:45:34 +1000

    Migrated submit-volt to v2

--- src/asvo/apiv2/client.rs
+++ src/asvo/apiv2/client.rs
@@ -34,9 +34,10 @@ use crate::obsid::Obsid;
 
 use super::error::Apiv2Error;
 use super::openapi::{
-    ApiLoginRequest, ApiLoginResponse, ConversionJobParams, DownloadJobParams, ErrorResponse,
-    ImagingJobFlow1Params, ImagingJobFlow2Params, JobDetailResponse, JobSubmittedResponse,
-    JobsByUserRequest, JobsByUserResponse, Login, TokenResponse, UserResponse, VoltageJobParams,
+    ApiLoginRequest, ApiLoginResponse, BeamformerJobParams, ConversionJobParams,
+    DownloadJobParams, ErrorResponse, ImagingJobFlow1Params, ImagingJobFlow2Params,
+    JobDetailResponse, JobSubmittedResponse, JobsByUserRequest, JobsByUserResponse, Login,
+    TokenResponse, UserResponse, VoltageJobParams,
 };
 
 const CONST_ENV_MWA_ASVO_API_KEY: &str = "MWA_ASVO_API_KEY";
@@ -620,6 +621,44 @@ impl AsvoClientv2 {
 
         let body = response.text()?;
         debug!("MWA ASVO v2 voltage_job response body: {}", body);
+        let resp: JobSubmittedResponse = serde_json::from_str(&body)?;
+        Ok(resp)
+    }
+
+    pub fn submit_beamformer_job(
+        &self,
+        params: &BeamformerJobParams,
+    ) -> Result<JobSubmittedResponse, Apiv2Error> {
+        debug!("Submitting a beamformer job to MWA ASVO v2");
+
+        let response = self
+            .client
+            .post(format!(
+                "{}/api/v2/beamformer_job",
+                get_asvo_server_address()
+            ))
+            .json(params)
+            .send()?;
+
+        if !response.status().is_success() {
+            let status = response.status();
+            let body = response.text().unwrap_or_default();
+            return Err(match serde_json::from_str::<ErrorResponse>(&body) {
+                Ok(err) => Apiv2Error::ApiError {
+                    error_code: err.error_code,
+                    message: err.message,
+                    detail: err.detail,
+                    suggestion: err.suggestion,
+                },
+                Err(_) => Apiv2Error::BadStatus {
+                    code: status,
+                    message: body,
+                },
+            });
+        }
+
+        let body = response.text()?;
+        debug!("MWA ASVO v2 beamformer_job response body: {}", body);
         let resp: JobSubmittedResponse = serde_json::from_str(&body)?;
         Ok(resp)
     }
